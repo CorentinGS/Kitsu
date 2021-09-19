a3fb433f99621c913a8569554f5c2983d5112ba9
Yume <yume.network@protonmail.com>
2021-09-19 08:24:55 +0200

    format file

--- src/general/ping.rs
+++ src/general/ping.rs
@@ -2,7 +2,6 @@ use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
-
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong").await?;
